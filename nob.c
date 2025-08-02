a610e4a0e39ae7d95432e8fc8bdc744df3e6267b
mrjiong <misjiong@gmail.com>
2025-08-02 13:17:30 +0800

    1

--- nob.c
+++ nob.c
@@ -15,6 +15,10 @@ int main(int argc, char **argv)
     if (!nob_cmd_run_sync_and_reset(&cmd)) return 1;
     nob_cmd_append(&cmd, "gcc", "-Wall", "-Wextra", DEBUG, "redmap.c", "-o", BUILD_FOLDER"redmap");
     if (!nob_cmd_run_sync_and_reset(&cmd)) return 1;
+    /* nob_cmd_append(&cmd, "./build/genmap"); */
+    /* if (!nob_cmd_run_sync_and_reset(&cmd)) return 1; */
+    /* nob_cmd_append(&cmd, "./build/redmap", "./the4.btb"); */
+    /* if (!nob_cmd_run_sync_and_reset(&cmd)) return 1; */
 
     return 0;
 }
